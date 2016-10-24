7379b6e9e02a2a89d8b5d34f694b0db58f27be47
madhuripatil06 <madhuri@thoughtworks.com>
2016-10-24 10:34:42 +0530

    added parameterized test to test runner

--- DFA/tests/DFATestsRunner.java
+++ DFA/tests/DFATestsRunner.java
@@ -1,21 +1,33 @@
+import org.json.simple.parser.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.io.IOException;
+import java.util.Arrays;
 
+@RunWith(Parameterized.class)
 public class DFATestsRunner {
-
-    @Test
-    public void run() throws Exception {
-        MyJSONFileReader myJsonFileReader = new MyJSONFileReader("example.json");
-        Result[] tests = myJsonFileReader.test();
-        for (Result test : tests) {
-            for (Object passingCase : test.passingCases) {
-                Assert.assertTrue(test.machine.run((String)passingCase));
-            }
-            for (Object failingCase : test.failingCases) {
-                Assert.assertFalse(test.machine.run((String) failingCase));
-            }
-        }
-
-
+    private final Machine machine;
+    private final Object[] passing_cases;
+    private final Object[] failing_cases;
+    public DFATestsRunner(Result result) {
+        this.machine = result.machine;
+        this.passing_cases = result.passingCases;
+        this.failing_cases = result.failingCases;
     }
+    @Test
+    public void dfaTestsRunner() {
+        for (Object passing_case : this.passing_cases) {
+            Assert.assertTrue(this.machine.run((String) passing_case));
+        }
+        for (Object failing_case : this.failing_cases) {
+            Assert.assertFalse(this.machine.run((String) failing_case));
+        }
+    }
+    @Parameterized.Parameters
+    public static Iterable data() throws IOException, ParseException {
+        return Arrays.asList(new MyJSONFileReader("").test());
+    };
 }
+
